2fc99b771fe46169f97e4db2dd709cee90044d1f
vishaldeep09 <09vishaldeep@gmail.com>
2024-09-12 14:55:25 +0530

    form implemented with personal details

--- src/app/components/create-student/create-student.component.ts
+++ src/app/components/create-student/create-student.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
   templateUrl: './create-student.component.html',
-  styleUrls: ['./create-student.component.scss']
+  styleUrls: ['./create-student.component.scss'],
 })
-export class CreateStudentComponent {
+export class CreateStudentComponent implements OnInit {
+  public studentForm!: FormGroup;
 
+  constructor() {}
+  ngOnInit(): void {
+    this.studentForm = new FormGroup({
+      name: new FormControl(''),
+      gender: new FormControl(''),
+      mobile: new FormControl(''),
+      email: new FormControl(''),
+      batch: new FormControl(''),
+    });
+  }
+
+  // -------Register Function----------------
+  register(){
+    console.log(this.studentForm.value);
+  }
 }
